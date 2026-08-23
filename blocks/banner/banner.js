baa79f4866ca0655680f09fd4b90e1100a88f0b8
Akash Gupta <akasgupta@adobe.com>
2026-08-23 22:59:27 +0530

    banner block

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,9 +1,5 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
-    
-    const wrapper = block.querySelector('div > div');
-    const bannerTitle = wrapper.querySelector('h1');
-    bannerTitle.className = 'banner-title';
-    
+  const wrapper = block.querySelector('div > div');
+  const bannerTitle = wrapper.querySelector('h1');
+  bannerTitle.className = 'banner-title';
 }
